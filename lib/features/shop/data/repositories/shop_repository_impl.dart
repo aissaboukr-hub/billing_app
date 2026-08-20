c0cad1f81e973bd7994dab8a791e6b3b21e5e327
Aissa Boukraa "Bejaia <Aissa.Bo@Techno-dz.com>
2026-08-20 16:30:30 +0100

    Mise à jour du projet

--- lib/features/shop/data/repositories/shop_repository_impl.dart
+++ lib/features/shop/data/repositories/shop_repository_impl.dart
@@ -18,12 +18,12 @@ class ShopRepositoryImpl implements ShopRepository {
       } else {
         // Return default shop if not found
         return const Right(Shop(
-            name: 'Dinesh Shop',
-            addressLine1: 'Samrajpet, Mecheri',
-            addressLine2: 'Salem - 636453',
-            phoneNumber: '+917010674588',
-            upiId: 'dineshsowndar@oksbi',
-            footerText: 'Thank you, Visit again!!!'));
+            name: 'CAISS ASW',
+            addressLine1: 'EDIMCO, BEJAIA',
+            addressLine2: '',
+            phoneNumber: '0550798297',
+            upiId: '',
+            footerText: 'Merci pour la visite'));
       }
     } catch (e) {
       return Left(CacheFailure(e.toString()));
